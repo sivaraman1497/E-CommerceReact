35b9494945bacaafc539b0285dc7438595d301e6
Siva Raman <sivaramantec21@gmail.com>
2024-10-06 20:50:22 +0530

    First commit 06-10-2024 (3)

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar/Navbar";
+import Body from "./components/Body/Body";
+
+export default function App() {
+  return (
+    <>
+        <Navbar />
+        <Body/>
+    </>
+  );
+}
